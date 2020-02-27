e70f1d582d7e4c28bb51d65d78b3e7d247fd1cb8
btbertram <S6108482@live.tees.ac.uk>
2020-02-27 17:59:28 +0000

    Created Database and Unity Project Prototype
    -Created a new scene for testing "TestingScene"
    -Added Plugins folder
    --Added SQLite DLLs
    -Created Database in Assets Folder
    -Created Scripts Folder
    --Created TestScript which connects to the database in the Assets
    Folder, sends and SQL SELECT query, and displays information in the
    Unity Log

---
+++ GamesProgrammingProject/Assets/Scripts/TestScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Data;
+using Mono.Data.Sqlite;
+
+public class TestScript : MonoBehaviour
+{
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        //SQLite doesn't work directly with LINQ. As a result, we'll need to work with more generic implementaitons.
+
+        string connectionString = "URI=file:" + Application.dataPath + "/prototypeDB.db";
+        IDbConnection dbConnection;
+        dbConnection = new SqliteConnection(connectionString);
+
+        dbConnection.Open();
+        IDbCommand dbCommand;
+        dbCommand = dbConnection.CreateCommand();
+
+
+        string selectQuery = "SELECT * FROM TestItem";
+
+        dbCommand.CommandText = selectQuery;
+        IDataReader dataReader = dbCommand.ExecuteReader();
+
+        while (dataReader.Read())
+        {
+            string itemName = dataReader.GetString(1);
+            int wealthValue = dataReader.GetInt32(2);
+
+            int id = dataReader.GetInt32(0);
+
+
+            Debug.Log("Name: " + itemName + ". Value: " + wealthValue + ". ID: " + id +".");
+        }
+
+        dataReader.Close();
+        dataReader = null;
+        dbCommand.Dispose();
+        dbCommand = null;
+        dbConnection.Close();
+        dbConnection = null;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
