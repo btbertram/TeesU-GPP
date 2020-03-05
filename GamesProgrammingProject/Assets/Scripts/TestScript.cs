98ac090b58e1878d7efb4ae7b8a9454c752c18db
btbertram <S6108482@live.tees.ac.uk>
2020-03-05 17:13:21 +0000

    Added preliminary user accounts implementation
    -Added create account function
    --Prelim/testing implementation
    -Added auth account function
    --Psudocoded
    -Added first version of user accounts table

--- GamesProgrammingProject/Assets/Scripts/TestScript.cs
+++ GamesProgrammingProject/Assets/Scripts/TestScript.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Data;
 using Mono.Data.Sqlite;
+using System.Security.Cryptography;
 
 public class TestScript : MonoBehaviour
 {
@@ -39,6 +40,28 @@ public class TestScript : MonoBehaviour
             Debug.Log("Name: " + itemName + ". Value: " + wealthValue + ". ID: " + id +".");
         }
 
+        string testusername = "testUser";
+        string testpasscode = "12345";
+
+        CreateAccount(testusername, testpasscode, dbConnection);
+
+        dbCommand.CommandText = "SELECT * FROM UserAccounts";
+
+        dbCommand.ExecuteReader();
+
+        while (dataReader.Read())
+        {
+            string userName = dataReader.GetString(1);
+            var salt = dataReader.GetValue(2);
+            var hash = dataReader.GetValue(3);
+
+            int id = dataReader.GetInt32(0);
+
+
+            Debug.Log("User: " + userName + ". Salt: " + salt + ". ID: " + id + ". Hash: " + hash + ".");
+        }
+
+
         dataReader.Close();
         dataReader = null;
         dbCommand.Dispose();
@@ -53,4 +76,51 @@ public class TestScript : MonoBehaviour
     {
         
     }
+
+    private void CreateAccount(string newUsername, string newPasscode, IDbConnection dbConnection)
+    {
+        //Generate Salt
+        System.Guid guid = System.Guid.NewGuid();
+
+        //Combine salt with newPasscode
+
+        byte[] encodedPasscode = System.Text.ASCIIEncoding.ASCII.GetBytes(newPasscode + guid.ToString() + newUsername);
+
+        //Hash Salted Passcode
+        ///Create Hashgen
+
+        SHA256 sHA256 = SHA256.Create();
+
+        string finalHash = sHA256.ComputeHash(encodedPasscode).ToString();
+
+      
+        //Insert query to database - new entry in user account table
+        ///Sends User and Hash
+        ///Need Database connection to do this - Should have a class that saves connection. Scriptable object?
+
+        string insertQuery = "INSERT into UserAccounts(username, hash, salt) VALUES( '" + newUsername + "', '" + finalHash + "', '" + guid + "');";
+        IDbCommand dbCommand = dbConnection.CreateCommand();
+        dbCommand.CommandText = insertQuery;
+
+        dbCommand.ExecuteNonQuery();
+        Debug.Log("Attempted Account created");
+
+        //DB side - Trigger after insert: Create new User data table
+    }
+
+    private void AuthAccount(string username, string passcode)
+    {
+        //Select Query to database - retrive salt + hash based on Username
+
+        //Combine salt + passcode
+
+        //Hash salted passcode
+
+        //compare generated hash vs DB stored hash
+
+        //Match: Success, login
+        //Fail: Display error message
+    }
+
+
 }
