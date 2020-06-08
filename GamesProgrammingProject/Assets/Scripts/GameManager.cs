09934eaef3dfbebf66e8df88d62931e01c57b62d
btbertram <brennantbertram@gmail.com>
2020-06-08 18:14:27 +0100

    User Session Creation -Created GameManager for future game state management and session management -Created UserSession to help session management -Renamed prototypeDB to GameDB -Added new table, UserStats, to GameDB

---
+++ GamesProgrammingProject/Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using UnityEngine;
+
+public sealed class GameManager
+{
+    private static GameManager instance = new GameManager();
+    public UserSession currentUser;
+
+
+    static GameManager()
+    {
+    }
+
+    private GameManager()
+    {
+    }
+
+    public static GameManager GmInstance
+    {
+        get { return instance; }
+    }
+    
+    public void SetUser(UserSession authenticatedUser)
+    {
+        currentUser = authenticatedUser;
+    }
+
+
+}
+
---
+++ GamesProgrammingProject/Assets/Scripts/UserSession.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+
+public class UserSession
+{
+    readonly int UserID;
+    readonly string Username;
+
+    public UserSession(int ID, string name)
+    {
+        UserID = ID;
+        Username = name;
+    }
+
+    public int GetID()
+    {
+        return UserID;
+    }
+
+    public string GetUsername()
+    {
+        return Username;
+    }
+
+}
