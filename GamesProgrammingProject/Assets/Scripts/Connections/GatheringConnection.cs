0d6efaa5eb29c06e10bb6874453a85f94071c820
btbertram <brennantbertram@gmail.com>
2020-07-14 17:24:58 +0100

    Gathering Points/Connections Additions -GatheringPoint: Added four new fields to store information about the point on the 'client' end -GatheringConneciton: Added function (Async)LoadGatheringPoints - Queries the database to get information about the gathering points and create a list of GameObjects with a GatheringPoint script -Database Table GatheringPoints: Modified posX, Y, Z to be REAL (float) datatype instead of Integer datatype to be compatible with Vector3 construction in Unity.

--- GamesProgrammingProject/Assets/Scripts/Connections/GatheringConnection.cs
+++ GamesProgrammingProject/Assets/Scripts/Connections/GatheringConnection.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Data;
+using System.Drawing;
 using System.Threading.Tasks;
 using UnityEngine;
 
@@ -10,6 +11,59 @@ public class GatheringConnection : MonoBehaviour
     string _userSessionUser;
     int _userSessionID;
     IDbConnection connection;
+
+
+    public async Task AsyncLoadGatheringPoints()
+    {
+        await new Task(() => LoadGatheringPoints());
+    }
+
+    private void LoadGatheringPoints()
+    {
+        ConnectionManager.OpenInstanceConnection();
+
+        IDbCommand dbCommand = ConnectionManager.GetConnection().CreateCommand();
+        string selectGatheringPoints = "SELECT * GatheringPoints";
+        dbCommand.CommandText = selectGatheringPoints;
+        IDataReader dataReader = dbCommand.ExecuteReader();
+
+        List<GameObject> gatheringPointObjects = new List<GameObject>();
+        int pointID;
+        EGatherPointType type;
+        float posX;
+        float posY;
+        float posZ;
+        int loopcounter = -1;
+        //Create a gameobject with the component, then set things on the component.
+
+        while (dataReader.Read())
+        {
+            loopcounter += 1;
+            var newgp = new GameObject("gatheringPoint" + loopcounter, typeof(GatheringPoint));
+            var gpscript = newgp.GetComponent<GatheringPoint>();
+            pointID = dataReader.GetInt32(0);
+            type = (EGatherPointType)dataReader.GetInt32(1);
+            posX = dataReader.GetFloat(2);
+            posY = dataReader.GetFloat(3);
+            posZ = dataReader.GetFloat(4);
+
+            var posLoad = new Vector3(posX, posY, posZ);
+
+            gpscript.LoadPoint(pointID, type, posLoad);
+
+            gatheringPointObjects.Add(newgp);
+        }
+
+
+        ConnectionManager.CloseInstanceConnection();
+
+        //Use information from the database to create a number of new game objects.
+        //Each object will be assigned a matching id, selected from the database
+        //That ID will be used to query for information from the database when necessary
+        //Each object will be positioned, and assigned a type based on information in the database. 
+
+
+    }
 
     public async Task<long> AsyncQueryGatherTime(int gatherPointID)
     {
@@ -52,4 +106,6 @@ public class GatheringConnection : MonoBehaviour
     {
         
     }
+
+
 }
--- GamesProgrammingProject/Assets/Scripts/Gameplay/GatheringPoint.cs
+++ GamesProgrammingProject/Assets/Scripts/Gameplay/GatheringPoint.cs
@@ -1,11 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using UnityEngine;
 
 public class GatheringPoint : MonoBehaviour
 {
+    int _pointID;
+    EGatherPointType _type;
+    Vector3 _pos;
+    bool _isActive;
 
-     
+
+    public void LoadPoint(int ID, EGatherPointType type, Vector3 pos)
+    {
+        _pointID = ID;
+        _type = type;
+        _pos = pos;
+    }
 
     // Start is called before the first frame update
     void Start()
