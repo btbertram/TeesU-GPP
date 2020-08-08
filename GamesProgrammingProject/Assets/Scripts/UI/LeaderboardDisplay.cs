09cf91782c1fd6ff2af6e650987180a932223ef6
btbertram <brennantbertram@gmail.com>
2020-08-08 14:57:48 +0100

    Leaderboards System Implementation Start
    OverworldScene.unity
    -Added:
    --Leaderboard Canvas GameObject. Navigated to from pause menu canvas, this canvas displays stats from the database in a gamified way.
    
    (NEW) LeaderboardDisplayBox.prefab : A unity UI prefab used to populate the leaderboard content in the Leaderboard Canvas scroll view.
    
    (NEW) LeaderboardDisplay.cs : A unity script that helps process and display information from the database in the unity UI system.
    -Similar structure to AchievementDisplay.cs in terms of functions and fields. I'd have both of these inherit from a parent class as part of a refactor. Class is still in progress.

---
+++ GamesProgrammingProject/Assets/Scripts/UI/LeaderboardDisplay.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeaderboardDisplay : MonoBehaviour
+{
+
+    RectTransform rectTransform;
+    public GameObject leaderboardDisplayBoxPrefab;
+    public float leaderboardDisplayBoxSize = 100;
+    List<GameObject> LeaderRowBoxes = new List<GameObject>();
+
+    GameObject LoadAddLeaderRowToContent()
+    {
+        rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, rectTransform.rect.height + leaderboardDisplayBoxSize);
+        var newLeaderBox = GameObject.Instantiate(leaderboardDisplayBoxPrefab, this.transform);
+
+        ConnectionManager.GetCMInstance();
+
+        return newLeaderBox;
+
+    }
+
+    void Awake()
+    {
+        rectTransform = this.gameObject.GetComponent<RectTransform>();
+        LoadAddLeaderRowToContent();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
